8ae9131cadf02f095718581d9a3b72bf52003d83
Yorch <jorgelitox15@gmail.com>
2025-10-05 23:08:51 -0500

    Added Sku controllers index and create

--- db/migrate/20251006030932_create_skus.rb
+++ db/migrate/20251006030932_create_skus.rb
@@ -2,6 +2,7 @@ class CreateSkus < ActiveRecord::Migration[8.0]
   def change
     create_table :skus do |t|
       t.string :description, null: false
+      t.string :location, null: false
       t.string :part_number, null: false
       t.timestamps
     end
--- db/migrate/20251006031204_create_item_locations.rb
+++ db/migrate/20251006031204_create_item_locations.rb
@@ -1,8 +1,8 @@
 class CreateItemLocations < ActiveRecord::Migration[8.0]
   def change
     create_table :item_locations do |t|
-      t.belongs_to :sku, :index: true, foreign_key: true
-      t.belongs_to :location, :index: true, foreign_key: true
+      t.belongs_to :sku, index: true, foreign_key: true
+      t.belongs_to :location, index: true, foreign_key: true
       t.timestamps
     end
   end
